470d2c5f56d3ee4a2c14dfe6a0ad56b11edd305f
Michael Oryl <michael@oryl.com>
2015-05-14 16:39:49 -0400

    Defined new dark colors

--- modules/rollo/colors.js
+++ modules/rollo/colors.js
@@ -16,12 +16,20 @@ module.exports.parseColor = function(color) {
 function parseColorString(color) {
   var colors = {
     red: 0xff0000,
+    darkred: 0x880000,
     green: 0x00ff00,
+    darkgreen: 0x008800,
     blue: 0x0000ff,
+    darkblue: 0x000080,
     orange: 0xffa500,
+    darkorange: 0x885200,
     purple: 0x800080,
-    white: 0xffffff,
+    darkpurple: 0x400040,
     yellow: 0xffff00,
+    darkyellow: 0x888800,
+    white: 0xffffff,
+    gray: 0x808080,
+    darkgray: 0xffffff,
     none: 0x000000,
     off: 0x000000
   };
